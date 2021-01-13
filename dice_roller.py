c73715333d08a9970122b07d019fa3c570ac429e
Slobodan Jovanovic <freeman81yu@yahoo.com>
2021-01-13 01:49:56 +0100

    Added input for dice roller

--- dice_roller.py
+++ dice_roller.py
@@ -2,15 +2,18 @@ import random
 
 def main():
 
-  dice_rolls = 2
+  #dice_rolls = 2
   dice_sum = 0
 
+  dice_rolls = int(input('How many dice would you like to rool? '))
+  dice_size = int(input('How many sides are in the dice? '))
+
   for i in (0, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
     if roll == 1:
       print(f'You rolled a {roll}! Critical Fail')
-    elif roll == 6:
+    elif roll == dice_size:
       print(f'You rolled a {roll}! Critical Success!')
     else:
       print(f'You rooled a {roll}')
